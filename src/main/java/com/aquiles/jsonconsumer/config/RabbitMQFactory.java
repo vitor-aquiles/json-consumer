8b2139baa4ff3a32107d6a450ed8f0c07a943bd7
Vitor Aquiles <aquilessrd@hotmail.com>
2020-01-31 18:24:35 -0300

    Refactoring

--- src/main/java/com/aquiles/jsonconsumer/config/RabbitMQFactory.java
+++ src/main/java/com/aquiles/jsonconsumer/config/RabbitMQFactory.java
@@ -3,13 +3,17 @@ package com.aquiles.jsonconsumer.config;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RabbitMQFactory {
 
     private ConnectionFactory factory;
 
     public Channel getConnection() throws Exception{
         factory.setHost("localhost");
+        factory.setUsername("guest");
+        factory.setPassword("guest");
         try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()){
             return channel;
         }
